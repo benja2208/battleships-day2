5d74e068efdc5d0c98cdc77e9d5d28945284c02e
Aftermaths <fnzzhou@gmail.com>
2015-07-27 17:10:26 +0100

    third

--- lib/ship.rb
+++ lib/ship.rb
@@ -9,8 +9,8 @@ class Ship
   def position (num)
     letter = num[0]
     nums = num[1..-1]
-    fail 'Outside the board' unless letter != /^[A-J]/
-    fail 'Outside the board' if nums != /[1-10]/
+    fail 'Outside the board' unless letter =~ /^[A-J]/
+    fail 'Outside the board' unless nums.to_i <= 10
     @pos = num
   end
 end
