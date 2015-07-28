18ca54e1c86a6f9a2275e03b438799992f5a4588
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-28 16:03:19 +0100

    Ugly NSWE

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,9 @@
 require_relative 'ship'
 
+
 class Board
   DEFAULT_SIZE = 10 
-  attr_reader :size
-  attr_accessor :b
-  attr_accessor :used_coordinates
-  attr_reader :n_of_X
+  attr_reader :size, :b, :used_coordinates,:n_of_X
 
   def initialize (size_board = DEFAULT_SIZE)
       @b = Array.new(size_board) {Array.new(size_board,'0')}
@@ -20,16 +18,53 @@ class Board
     end 
   end 
 
-  def place (ship,x,y) 
-    fail 'Outside the board' unless x <= @size && y <= @size
-    fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + y.to_s) 
-    @n_of_X += 1
-    @used_coordinates << x.to_s + y.to_s
-    b[y - 1][x - 1] = 'X'
+  def place (ship,x,y,orientation) 
+  case orientation 
+    when "S" 
+      fail 'Outside the board' unless x <= size && y <= size
+      fail 'Outside the board' unless x <= size && y+1 <= size
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + y.to_s) 
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + (y+1).to_s) 
+      @n_of_X += 2
+      @used_coordinates << x.to_s + y.to_s
+      @used_coordinates << x.to_s + (y+1).to_s
+      b[y][x] = 'X'
+      b[y+1][x] = 'X'
+    when "N"
+      fail 'Outside the board' unless x <= size && y <= size
+      fail 'Outside the board' unless x <= size && y-1 <= size
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + y.to_s) 
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + (y-1).to_s) 
+      @n_of_X += 2
+      @used_coordinates << x.to_s + y.to_s
+      @used_coordinates << x.to_s + (y-1).to_s
+      b[y][x] = 'X'
+      b[y-1][x] = 'X'
+    when "E"
+      fail 'Outside the board' unless x <= size && y <= size
+      fail 'Outside the board' unless x+1 <= size && y <= size
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + y.to_s) 
+      fail 'Those coordinates are already used!' if @used_coordinates.include? ((x+1).to_s + y.to_s) 
+      @n_of_X += 2
+      @used_coordinates << x.to_s + y.to_s
+      @used_coordinates << (x+1).to_s + y.to_s
+      b[y][x] = 'X'
+      b[y][x+1] = 'X'
+    when "W"
+      fail 'Outside the board' unless x <= size && y <= size
+      fail 'Outside the board' unless x-1 <= size && y <= size
+      fail 'Those coordinates are already used!' if @used_coordinates.include? (x.to_s + y.to_s) 
+      fail 'Those coordinates are already used!' if @used_coordinates.include? ((x-1).to_s + y.to_s) 
+      @n_of_X += 2
+      @used_coordinates << x.to_s + y.to_s
+      @used_coordinates << (x-1).to_s + y.to_s
+      b[y][x] = 'X'
+      b[y][x-1] = 'X'
+  end 
   end
 
   def fire(x,y)
-    b[y - 1][x - 1] == '0' ? b[y - 1][x - 1] = 'M' : b[y - 1][x - 1] = 'H' 
+    b[y][x] == '0' ? b[y][x] = 'M' : b[y][x] = 'H' 
   end
   
   def number_of_hits
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,25 +13,25 @@ describe Board do
   end 
 
   describe '#placing ' do 
-    it { is_expected.to respond_to(:place).with(3).argument }
+    it { is_expected.to respond_to(:place).with(4).argument }
 
     it 'places a ship in its position' do
-      subject.place(ship,1,1)
+      subject.place(ship,0,0,'S')
       expect(subject.b[0][0]).to eq 'X'
     end
 
     it 'will not place a ship beyond the board' do
-      expect{subject.place ship,subject.size+1,subject.size+1}.to raise_error('Outside the board')
+      expect{subject.place ship,subject.size,subject.size,'S'}.to raise_error('Outside the board')
     end
 
     it 'should increase the count of ships' do 
-      subject.place(ship,1,1)
-      expect(subject.n_of_X).to eq 1
+      subject.place(ship,1,1,'S')
+      expect(subject.n_of_X).to eq 2
     end 
 
     it 'cannot place a ship on another one' do 
-      subject.place(ship,1,1)
-      expect{subject.place(ship,1,1)}.to raise_error 'Those coordinates are already used!'
+      subject.place(ship,1,1,'S')
+      expect{subject.place(ship,1,1,'S')}.to raise_error 'Those coordinates are already used!'
     end 
   end 
 
@@ -39,20 +39,20 @@ describe Board do
     it { is_expected.to respond_to(:fire).with(2).argument }
 
     it "says 'H' for a hit" do
-      subject.place(ship,1,1)
+      subject.place(ship,1,1,'S')
       expect(subject.fire(1,1)).to eq 'H'
     end
 
     it "says 'M' for a miss" do
-      subject.place(ship,1,1)
+      subject.place(ship,1,1,'S')
       expect(subject.fire(2,2)).to eq 'M'
     end
   end 
   
   describe '#number_of_hits' do 
     it 'shows the number of hits' do 
-      subject.place(ship,3,3)
-      subject.place(ship,1,1)
+      subject.place(ship,3,3,'S')
+      subject.place(ship,1,1,'S')
       subject.fire(1,1)
       subject.fire(3,3)
       expect(subject.number_of_hits).to eq 2
@@ -61,10 +61,10 @@ describe Board do
 
   describe '#number_of_missed' do 
     it 'shows the number of misses' do 
-      subject.place(ship,3,3)
-      subject.place(ship,1,1)
+      subject.place(ship,3,3,'S')
+      subject.place(ship,1,1,'S')
       subject.fire(1,4)
-      subject.fire(3,4)
+      subject.fire(3,6)
       expect(subject.number_of_misses).to eq 2
     end 
   end 
@@ -72,9 +72,8 @@ describe Board do
 
   describe '#victory' do 
     it 'will return true when all ships are hit' do 
-      subject.place(ship,3,3)
-      subject.place(ship,1,1)
-      subject.fire(1,1)
+      subject.place(ship,3,3,'S')
+      subject.fire(3,4)
       subject.fire(3,3)
       expect(subject.victory?).to eq true
     end 
@@ -82,8 +81,8 @@ describe Board do
 
   describe '#log' do 
     it 'will show the numbers of misses and hits' do 
-      subject.place(ship,3,3)
-      subject.place(ship,1,1)
+      subject.place(ship,3,3,'S')
+      subject.place(ship,1,1,'S')
       subject.fire(1,1)
       subject.fire(3,2)
       expect(subject.log).to eq 'H:1, M:1'
